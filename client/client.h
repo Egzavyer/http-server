73526b84460461c757d39c4aebf2bedf6dff5da2
Egzavyer <xavier.lermusieaux@gmail.com>
2024-07-25 19:48:30 -0400

    Initial commit

---
+++ client/client.h
@@ -0,0 +1,10 @@
+#ifndef HTTP_SERVER_CLIENT_H
+#define HTTP_SERVER_CLIENT_H
+
+
+class Client {
+
+};
+
+
+#endif //HTTP_SERVER_CLIENT_H
---
+++ server/server.h
@@ -0,0 +1,10 @@
+#ifndef HTTP_SERVER_SERVER_H
+#define HTTP_SERVER_SERVER_H
+
+
+class Server {
+
+};
+
+
+#endif //HTTP_SERVER_SERVER_H
